073605689470cc7d49fb443b5a13e42a6d8c2aec
Inaldo Monteiro <inaldo.monteiro@inbec.com.br>
2020-03-27 15:52:42 -0300

    MVCPHP

---
+++ view/Index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+</head>
+<body>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Página inicial</h1>
+            <a href="/?pagina=login">Login</a>
+        </div>
+    </div>
+</body>
+</html>
